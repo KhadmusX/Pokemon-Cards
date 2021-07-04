7a563c7cb3e53f9fd23e61c1e3626eca15b5c1b8
KhadmusX <alex.adriel.amaya@gmail.com>
2021-07-04 04:25:31 -0300

    Added Default Port

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from db import initDb
-import card
+import card, os
 
 app = Flask(__name__)
 app.register_blueprint(card.public)
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
     initDb()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug = True)
 
